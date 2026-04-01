df5ea80bd4f5a44db50a19573873f1f77a64c7e5
gumelthemodeler <jcmnewman.gumel.atl@gmail.com>
2026-03-31 21:46:57 -0400

    Updated ReplicatedStorage/LoginData.lua

---
+++ ReplicatedStorage/LoginData.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+-- @ScriptType: ModuleScript
+local LoginData = {
+	[1] = { Type = "Dews", Amount = 1000 },
+	[2] = { Type = "Item", Name = "Cadet Training Blade", Amount = 1 },
+	[3] = { Type = "Item", Name = "Scout Training Manual", Amount = 3 },
+	[4] = { Type = "Dews", Amount = 5000 },
+	[5] = { Type = "Item", Name = "Standard Titan Serum", Amount = 5 },
+	[6] = { Type = "Item", Name = "Advanced ODM Gear", Amount = 1 },
+	[7] = { Type = "Item", Name = "Clan Blood Vial", Amount = 1 } 
+}
+return LoginData
