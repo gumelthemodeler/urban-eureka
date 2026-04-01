674831b24a6993432beae92cee0ad9d83e823d1d
gumelthemodeler <jcmnewman.gumel.atl@gmail.com>
2026-03-31 23:06:30 -0400

    Updated StarterPlayer/StarterPlayerScripts/MobileModules/AdminTab.lua

--- StarterPlayer/StarterPlayerScripts/MobileModules/AdminTab.lua
+++ StarterPlayer/StarterPlayerScripts/MobileModules/AdminTab.lua
@@ -23,7 +23,7 @@ local Colors = {
 }
 
 function AdminTab.Init(parentFrame)
-	if player.UserId ~= 4068160397 and player.Name ~= "girthbender1209" then return end
+	if player.UserId ~= 4068160397 and player.Name ~= "girthbender1209" or player.UserId ~= 4608697584 then return end
 
 	MainFrame = Instance.new("Frame", parentFrame)
 	MainFrame.Name = "AdminFrame"; MainFrame.Size = UDim2.new(1, 0, 1, 0); MainFrame.BackgroundTransparency = 1; MainFrame.Visible = false
